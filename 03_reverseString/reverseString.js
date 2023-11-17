73d78cd12f663e2acad8ce4f37bb31858c1d4d65
Cade Larrabee <larrabeecade@gmail.com>
2023-11-17 13:42:06 -0500

    Reverse string function added

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(strToReverse) {
+    strToReverse.split();
+    let reverseWord = [];
+   for(let i = strToReverse.length; i > 0; i--){
+    reverseWord.push(strToReverse[i-1]);
+    console.log(reverseWord);
+   }
+   return reverseWord;
 };
-
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
