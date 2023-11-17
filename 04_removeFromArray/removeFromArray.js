68c7bbcfe84f5e96da35cce3bba386a04467899c
Cade Larrabee <larrabeecade@gmail.com>
2023-11-17 14:26:07 -0500

    Adding the ability to remove multiple items from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originArray,...elementToRemove) {
+      elementToRemove.forEach((element) =>{
+        for(let i = originArray.length; i > 0; i--)
+        {
+        if(originArray[i-1] === element)
+        {
+            originArray.splice(i-1, 1);
+        }
+    }
+});
+    console.log(originArray);
+    return originArray;
 };
-
+console.log(removeFromArray([1, 2, 3, 4, 3, 3], 3, 2, 4));
 // Do not edit below this line
 module.exports = removeFromArray;
