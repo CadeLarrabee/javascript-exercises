e011dc17b01abe2cdbbd8aa75231333796e2e08c
Cade Larrabee <larrabeecade@gmail.com>
2023-11-17 13:06:08 -0500

    Added ability to repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(strToRepeat, numOfReps) {
+    let outputString = "";
+    if(numOfReps < 0){
+        return 'ERROR';
+    }
+    for(let i = 0; i < numOfReps; i++){
+        outputString += strToRepeat;
+    }
+    return outputString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
