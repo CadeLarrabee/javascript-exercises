6ed6c21da10f06bb521bfa26d2e78e8e434167ef
Cade Larrabee <larrabeecade@gmail.com>
2023-11-18 16:50:03 -0500

    negative number and non-integer support

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(numInputOne, numInputTwo) {
+    let sumOfAllNums = 0;
+    let largerNum;
+    let smallerNum;
 
+    if(numInputOne < 0 || numInputTwo < 0){
+        return "ERROR";
+    }
+    if(!Number.isInteger(numInputOne) || !Number.isInteger(numInputTwo)){
+        return "ERROR";
+    }
+
+    if(numInputOne < numInputTwo){
+        largerNum = numInputTwo;
+        smallerNum = numInputOne;
+    }
+    else{
+        largerNum = numInputOne;
+        smallerNum = numInputTwo;
+    }
+    
+    for(i = smallerNum; i <= largerNum;i++){
+        sumOfAllNums += i;
+    }
+    console.log(sumOfAllNums);
+    return sumOfAllNums;
 };
-
+sumAll(123,1);
 // Do not edit below this line
 module.exports = sumAll;
