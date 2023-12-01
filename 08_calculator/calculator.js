06b0ff23da5b97e430a038893a6e7931d50c332f
Cade Larrabee <larrabeecade@gmail.com>
2023-12-01 18:57:10 -0500

    adding calculator function
    adding calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (numInputOne, numInputTwo) {
+  return numInputOne + numInputTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function (numInputOne, numInputTwo) {
+  return numInputOne - numInputTwo;
 };
 
-const sum = function() {
-	
+const sum = function (arrayInput) {
+  let sum = 0;
+  arrayInput.forEach((item) => (sum += parseInt(item)));
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arrayInput) {
+  let sum = arrayInput[0];
+  arrayInput.forEach(function (item, index) {
+    if (index != 0) {
+      sum *= parseInt(item);
+    }
+  });
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (intInput, toPower) {
+  let sum = intInput;
+  for (let i = 0; i < toPower - 1; i++) {
+    sum *= intInput;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function (intInput) {
+  let sum = intInput;
+  if (intInput == 0) {
+    return 1;
+  }
+  for (let i = intInput - 1; i > 0; i--) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
