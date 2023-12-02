c8cfc0213de577e1cf51d10ba11907f9ad205552
Cade Larrabee <larrabeecade@gmail.com>
2023-12-02 13:51:04 -0500

    completing the fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (numOfRepeats) {
+  numOfRepeats = parseInt(numOfRepeats);
+  if (numOfRepeats < 0) {
+    return "OOPS";
+  }
 
+  let fibA = 1;
+  let fibB = 1;
+  let sum = 1;
+  for (let i = 2; i < numOfRepeats; i++) {
+    sum = fibA + fibB;
+    fibA = fibB;
+    fibB = sum;
+  }
+  return sum;
 };
 
 // Do not edit below this line
