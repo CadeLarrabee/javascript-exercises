1e762d3626af0849a9fb739acb607a47426c4e35
Cade Larrabee <larrabeecade@gmail.com>
2023-12-01 22:43:56 -0500

    adding palindrome, formatting and reversing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (stringPalin) {
+  //Fix the formatting of the input
+  stringPalin = stringPalin.replace(/[\p\s{P}$+<=>^,.!|~]/g, "").toLowerCase();
+  let originString = stringPalin.split("").reverse().join("");
 
+  if (originString === stringPalin) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+palindromes("A car, a man, a maraca.");
 // Do not edit below this line
 module.exports = palindromes;
